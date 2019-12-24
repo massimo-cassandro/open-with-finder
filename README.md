f50d15940cdfc5fc21b9c901b4f89bcd3821baa6
Massimo Cassandro <massimo.cassandro@gmail.com>
2019-12-24 14:31:01 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# open-with-finder
-vscode extension to open files using their default OSX app 
+# Open with finder
+
+Vscode extension to open files using their default OSX app.
+
+Right clic the file to open within the explorer window and choose "Open with Finder".
+
---
+++ extension.js
@@ -0,0 +1,47 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with  registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand('mazz.openWithFinder', function (e) {
+
+    //console.log('e.path: ' + e.path );
+
+    const cp = require('child_process')
+    cp.exec(`open "${e.path}"`, (err, stdout, stderr) => {
+      // console.log('stdout: ' + stdout);
+      // console.log('stderr: ' + stderr);
+      vscode.window.showInformationMessage('Opening “' + e.path.split('/').pop() + '”');
+      if (err) {
+        vscode.window.showErrorMessage(
+          'e.path: ' + e.path + '\n'+
+          'error: ' + err + '\n'+
+          'stdout: ' + stdout + '\n'+
+          'stderr: ' + stderr
+        );
+      }
+    });
+
+});
+
+  context.subscriptions.push(disposable);
+}
+exports.activate = activate;
+
+// this method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+  activate,
+  deactivate
+}
